2d279ff2c8cd2ef927a7c1b404603a0fe3a043f4
rodperdan <47697303+rodperdan@users.noreply.github.com>
2019-02-16 19:32:28 +0100

    Add files via upload

---
+++ 02_list-dict.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+nombres=["paco","maria","juan"]
+
+
+# In[3]:
+
+
+numeros=[1,2,3]
+
+
+# In[4]:
+
+
+decimales=[1.1,2.2,3.3]
+
+
+# In[5]:
+
+
+mezcla=["mariano",4,5.5]
+
+
+# In[14]:
+
+
+type(nombres),type(numeros),type(decimales),type(mezcla)
+
+
+# In[15]:
+
+
+len(nombres),len(numeros),len(decimales),len(mezcla)
+
+
+# In[24]:
+
+
+nombre=nombres[-1]
+
+
+# In[25]:
+
+
+numero=numeros[-1]
+
+
+# In[26]:
+
+
+decimal=decimales[-1]
+
+
+# In[28]:
+
+
+print("El último valor de las listas es: "+nombre+", "+str(numero)+" y "+str(decimal))
+
+
+# In[29]:
+
+
+pelis={"Spielberg":"La Lista de Schindler","Curtis":"Coraza Negra","Berlanga":"La Vaquilla"}
+
+
+# In[30]:
+
+
+print(pelis)
+
+
+# In[31]:
+
+
+pelis.keys()
+
+
+# In[32]:
+
+
+pelis.values()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
