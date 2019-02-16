f6205de9b3a6653f0f89eb789952637bdba01984
rodperdan <47697303+rodperdan@users.noreply.github.com>
2019-02-16 19:44:07 +0100

    Add files via upload

---
+++ 03_personal-info.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+nombre=input("¿Cuál es tu nombre?")
+
+
+# In[ ]:
+
+
+apellido=input("¿y tu primer apellido?")
+
+
+# In[ ]:
+
+
+tlf=input("¿Me podrías dar tu tlf?")
+
+
+# In[ ]:
+
+
+print("Tus datos personales son: "+nombre+" "+apellido+" y teléfono "+str(tlf))
+
+
+# In[ ]:
+
+
+
+
